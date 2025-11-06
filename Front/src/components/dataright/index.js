7e16986ac2c6284fd2a95b2f84053d206e0dae5c
Nikolas Railan <nikolasrailanmp@gmail.com>
2025-11-06 18:21:49 -0300

    taste

--- Front/src/components/dataright/index.js
+++ Front/src/components/dataright/index.js
@@ -12,9 +12,7 @@ import {
   Submit
 } from './style';
 
-const socket = io("http://10.20.21.220:3001");
-
-
+const socket = io("http://10.20.15.104:3001");
 
 
 export default function DataLeft() {
